c9ecf5ce4182a8aafbd33252686fae82c13bed26
carokartu <carokrstulovich@gmail.com>
2021-05-25 15:11:46 -0300

    web desing finish

---
+++ code/webR3.js
@@ -0,0 +1,25 @@
+const modalButton = document.getElementById("modal_video_open")
+const overlayModal = document.getElementById("overlayModal")
+const openModal = document.getElementById("button_modal")
+const closeModal = document.getElementById("close")
+
+
+
+  closeModal.onclick = () => {
+    overlayModal.classList.add("ocultar")
+    document.body.classList.remove("no-scroll")
+    overlay.classList.add("ocultar")
+    document.body.classList.remove("no-scroll")
+   
+  }
+  
+
+
+ openModal.onclick = () => {
+
+    overlayModal.classList.remove("ocultar")
+    document.body.classList.add("no-scroll")
+       
+
+    
+}
